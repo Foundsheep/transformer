0cab623273bd792d94dbf6a2d0393af65136042d
Foundsheep <moondeokjong@gmail.com>
2022-11-22 10:38:44 +0900

    inital commit

---
+++ dataset.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d47c4aea9e3b8d59bc551c98657807e49a03062a21069b0bf02a404b98507a7f
+size 855
---
+++ requirements.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:65f4d1e071b6fc2ddf2a48f05984260ee77eece77f213855950959f1bb55cfea
+size 352
