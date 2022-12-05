5d513d5a32bb31e1887b0710fb2dde8c0cd77cb8
Foundsheep <moondeokjong@gmail.com>
2022-12-05 15:00:49 +0900

    updated the process to bring datasets

--- dataset.py
+++ dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d47c4aea9e3b8d59bc551c98657807e49a03062a21069b0bf02a404b98507a7f
-size 855
+oid sha256:4c605691c7412d3937bb2fc40e73e698fe6b6e02bdc4742d0e62526af93a7fda
+size 1884
