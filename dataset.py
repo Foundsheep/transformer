3abe7bd9a655ef1dc99f8efb12f0e995fab447d3
Foundsheep <moondeokjong@gmail.com>
2022-12-05 15:00:49 +0900

    updated the process to bring datasets

--- dataset.py
+++ dataset.py
@@ -20,7 +20,36 @@ for pt_examples, en_examples in train_examples.batch(3).take(1):
     for en in en_examples.numpy():
         print(en.decode('utf-8'))
 
-# -- Set up the tokenizer
+# -- Download and load the tokenizer
+model_name = 'ted_hrlr_translate_pt_en_converter'
+tf.keras.utils.get_file(
+    f'{model_name}.zip',
+    f'https://storage.googleapis.com/download.tensorflow.org/models/{model_name}.zip',
+    cache_dir='.', cache_subdir='', extract=True
+)
+
+tokenizers = tf.saved_model.load(model_name)
+
+# -- Set the MAX_TOKEN number
+# TODO : might be better to save it once in a config file
+lengths = []
+for pt_examples, en_examples in train_examples.batch(2048):
+    pt_tokens = tokenizers.pt.tokenize(pt_examples)
+    lengths.append(pt_tokens.row_lengths())
+
+    en_tokens = tokenizers.en.tokenize(en_examples)
+    lengths.append(en_tokens.row_legnths())
+
+lengths = np.concatenate(lengths)
+q3 = np.quantile(legnths, 0.75)
+print(f"Tokenised dataset length max : {legnths.max()}")
+print(f"Tokenised dataset length min : {legnths.min()}")
+print(f"Tokenised dataset length mean : {legnths.mean()}")
+print(f"Tokenised dataset length median : {np.median(lengths)}")
+print(f"Tokenised dataset length MAX_TOKEN : {q3}")
+
+MAX_TOKEN = int(q3)
+
 
 # -- Preprocessing steps needed
 # 1. shuffle
